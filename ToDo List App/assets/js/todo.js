b287142bf6fd24c1b057cfa385f0a2414c378da5
Santosh Gunashekar <santoshguna001@gmail.com>
2020-05-24 12:21:26 +0530

    Completed To-Do List App

--- ToDo List App/assets/js/todo.js
+++ ToDo List App/assets/js/todo.js
@@ -1,10 +1,22 @@
-$('li').click(function() {
+$('ul').on('click', 'li', function() {
     $(this).toggleClass("completed");
 })
 
-$('ul li span').click(function(event) {
+$('ul').on('click', 'li span', function(event) {
     $(this).parent().fadeOut(500, function() {
         $(this).remove();
     })
     event.stopPropagation();
+})
+
+$("input[type='text']").on('keypress', function(event) {
+    if (event.which === 13) {
+        var toDoText = $(this).val();
+        $('ul').append("<li><span><i class='fa fa-trash'></i></span> " + toDoText + "</li > ")
+        $(this).val("");
+    }
+})
+
+$('.fa-plus').click(function() {
+    $('input[type="text"]').fadeToggle();
 })
