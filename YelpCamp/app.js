9be0b1a0576c3dded3348372f8c032df62c684c4
santoshguna001 <santoshguna001@gmail.com>
2020-06-17 15:11:45 +0530

    YelpCamp Comments With Styling

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -10,6 +10,7 @@ var express = require('express'),
 seedDB();
 mongoose.connect("mongodb://localhost:27017/YelpCamp", { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 
