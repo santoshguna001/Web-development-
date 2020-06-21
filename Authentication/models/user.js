2a8ab5cf12028bbe18540b201ad4da48240e75eb
santoshguna001 <santoshguna001@gmail.com>
2020-06-21 11:35:01 +0530

    Change user schema

--- Authentication/models/user.js
+++ Authentication/models/user.js
@@ -6,6 +6,6 @@ var userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(passportLocalMongoose)
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', userSchema);
